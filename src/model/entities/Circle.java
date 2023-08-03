f1cc0d7c3e7606fae630464c960a5bff41e2d8cb
Bruno Pires <brunopires.ctto@gmail.com>
2023-08-03 16:52:25 -0300

    Solution with Interface and Abstract Class

--- src/model/entities/Circle.java
+++ src/model/entities/Circle.java
@@ -2,7 +2,7 @@ package model.entities;
 
 import model.enums.Color;
 
-public class Circle extends Shape{
+public class Circle extends AbstractShape{
 	
 	private Double radius;
 
@@ -10,12 +10,15 @@ public class Circle extends Shape{
 		super(color);
 		this.radius = radius;
 	}
+
 	public Double getRadius() {
 		return radius;
 	}
+
 	public void setRadius(Double radius) {
 		this.radius = radius;
 	}
+
 	@Override
 	public double area() {
 		return Math.pow(radius, 2)*3.14;
--- src/model/entities/Rectangle.java
+++ src/model/entities/Rectangle.java
@@ -1,16 +1,18 @@
 package model.entities;
 
-public class Rectangle extends Shape{
+import model.enums.Color;
+
+public class Rectangle extends AbstractShape{
 	
 	private Double width;
 	private Double height;
 
-	public Rectangle(model.enums.Color color, Double width, Double height) {
+		public Rectangle(Color color, Double width, Double height) {
 		super(color);
 		this.width = width;
 		this.height = height;
 	}
-	
+
 	public Double getWidth() {
 		return width;
 	}
